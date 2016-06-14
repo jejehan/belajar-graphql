738323d68b11932493a8e0162d0bc3927ab0a94d
chasingrainbow <seventpoint@gmail.com>
2016-06-15 02:00:28 +0700

    ambil dan tampilkan data dari array

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 var Schema = require("graph.ql");
 
+var characters = {
+    1: {
+        id: 1,
+        name: 'Jason'
+    },
+    2: {
+        id: 2,
+        name: 'Bourne'
+    },
+    3: {
+        id: 3,
+        name: 'zeihan',
+    },
+    4: {
+        id: 4,
+        name: 'aulia',
+    }
+
+}
+
 var schema = Schema(`
 
     scalar Date
 
     type Character {
+        id: Int
         name: String!
         homeworld: Planet
         films: [Film]
@@ -13,7 +34,7 @@ var schema = Schema(`
     type Film {
         title: String!
         producers: [String]
-        characters: [Character]
+        characters(): [Character]
         release_date: Date
     }
 
@@ -37,7 +58,11 @@ var schema = Schema(`
 
     },
     Film: {
-
+        characters (film, args){
+            return film.character_ids.map(function (id) {
+                return characters[id]
+            });
+        }
     },
     Planet: {
 
@@ -51,7 +76,8 @@ var schema = Schema(`
             return {
                 title: 'Jason Bourne',
                 producers: ['matt','damon','zeihan'],
-                release_date: '2006-01-07'
+                release_date: '2006-01-07',
+                character_ids: [1,3,4]
             }
         },
         find_character (query, args){
@@ -60,12 +86,17 @@ var schema = Schema(`
     }
 });
 
+// query yang akan ditampilkan
 schema(`
     query find($film: Int){
         find_film(id: $film){
             title
             producers
             release_date
+            characters {
+                id
+                name
+            }
         }
     }
 `,{
